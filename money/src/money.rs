d865f061fef184e14d6244ed8a38e4cd57e59206
kiharito <kihara13611.c7d7@gmail.com>
2023-11-22 22:59:29 +0900

    wip:cp5

--- money/src/money.rs
+++ money/src/money.rs
@@ -24,6 +24,32 @@ impl Dollar {
     }
 }
 
+#[derive(Debug)]
+pub struct Franc {
+    amount: u32,
+}
+
+impl PartialEq for Franc {
+    fn eq(&self, franc: &Self) -> bool {
+        self.amount == franc.amount()
+    }
+}
+
+impl Franc {
+    pub fn new(amount: u32) -> Self {
+        Franc { amount }
+    }
+    pub fn times(&self, multiplier: u32) -> Franc {
+        Franc::new(self.amount * multiplier)
+    }
+    pub fn amount(&self) -> u32 {
+        self.amount
+    }
+    pub fn equals(&self, franc: Self) -> bool {
+        self.eq(&franc)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +65,12 @@ mod tests {
     fn test_equality() {
         assert!(Dollar::new(5).equals(Dollar::new(5)));
         assert!(!Dollar::new(5).equals(Dollar::new(6)))
+    }
+
+    #[test]
+    fn test_franc_multiplication() {
+        let five = Franc::new(5);
+        assert_eq!(Franc::new(10), five.times(2));
+        assert_eq!(Franc::new(15), five.times(3));
     }
 }
